477522d55f3719056412c181f2e5f66c496d2439
Emery Tate <tateejt@gmail.com>
2019-12-04 18:45:46 +0000

    Done.

--- config.ru
+++ config.ru
@@ -1,13 +1,9 @@
-
 require './config/environment'
+require './app/controllers/application_controller'
 
-begin
-  fi_check_migration
-
-  use Rack::MethodOverride
-
-  run ApplicationController
-rescue ActiveRecord::PendingMigrationError => err
-  STDERR.puts err
-  exit 1
+if ActiveRecord::Migrator.needs_migration?
+  raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
+
+use Rack::MethodOverride
+run ApplicationController
--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,8 @@
+
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
-def fi_check_migration
-  begin
-    ActiveRecord::Migration.check_pending!
-  rescue ActiveRecord::PendingMigrationError
-    raise ActiveRecord::PendingMigrationError.new "Migrations are pending.\nTo resolve this issue, run: \nrake db:migrate SINATRA_ENV=#{ENV['SINATRA_ENV']}"
-  end
-end
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
